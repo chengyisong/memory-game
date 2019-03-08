c868483c4be21b765f4a4de4a942e4922546c55c
CHENGYI SONG <carrie.song@beyondsoft.com>
2019-03-07 23:15:08 -0800

    Updates

---
+++ precache-manifest.daf1c3ad76ee81d4da50bda1482639d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memory-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "435a81855f49d55acd25",
+    "url": "/memory-game/static/js/runtime~main.6cb836fd.js"
+  },
+  {
+    "revision": "3bcf847a0ac7f7a8dc5d",
+    "url": "/memory-game/static/js/main.69b4c331.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/memory-game/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "3bcf847a0ac7f7a8dc5d",
+    "url": "/memory-game/static/css/main.a0e2774c.chunk.css"
+  },
+  {
+    "revision": "72ab993a02eaaca1ba05adf110dc086f",
+    "url": "/memory-game/index.html"
+  }
+];
